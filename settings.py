94d81c14874de10c3b7d594846edc491d453b5f6
pilotf369u-sys <pilotf369u@gmail.com>
2026-06-16 17:41:38 +0300

    Add MeshwarConfig to installed apps

--- settings.py
+++ settings.py
@@ -15,6 +15,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.MeshwarConfig', # أضف هذا السطر
 ]
 
 MIDDLEWARE = [
