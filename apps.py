6b56461d2f15832397fdb73a9e5e22a9072c7ac7
pilotf369u-sys <pilotf369u@gmail.com>
2026-06-16 17:56:20 +0300

    Fix indentation in MeshwarConfig class

--- apps.py
+++ apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
 class MeshwarConfig(AppConfig):
-    name = 'meshwar_system' # تأكد أن هذا الاسم يطابق اسم المجلد الرئيسي لمشروعك
-    label = 'meshwar_system'
+    name = 'meshwar_system'
