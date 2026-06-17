d1afe90a6ddaed6cc4a709452653399cf1c9a452
pilotf369u-sys <pilotf369u@gmail.com>
2026-06-17 16:06:04 +0300

    Redirect root URL to dashboard
    Add redirect from root URL to dashboard.

--- urls.py
+++ urls.py
@@ -1,9 +1,13 @@
 # coding: utf-8
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView # لاستيراد أداة التحويل التلقائي للروابط
 from views import customer_dashboard, upload_deposit_receipt
 
 urlpatterns = [
+    # توجيه الرابط الرئيسي للموقع تلقائياً إلى لوحة التحكم
+    path('', RedirectView.as_view(url='dashboard/', permanent=False)),
+    
     path('admin/', admin.site.urls),  # لوحة تحكم الإدارة
     path('dashboard/', customer_dashboard, name='customer_dashboard'),  # لوحة تتبع الزبون
     path('upload-receipt/', upload_deposit_receipt, name='upload_deposit_receipt'),  # رفع الحوالة
